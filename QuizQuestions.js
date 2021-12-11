96629a41a624c661078d79bf4b4bddd0334939d7
SANDEEP K <87005602+sandeepkris2001@users.noreply.github.com>
2021-12-12 03:18:46 +0530

    Add files via upload

---
+++ QuizQuestions.js
@@ -0,0 +1,68 @@
+let questions = [
+    {
+    numb: 1,
+    question: "The Curies' ____ collaboration helped to unlock the secrets of the atom.",
+    answer: "friendly",
+    options: [
+      "friendly",
+      "competitive",
+      "courteous",
+      "industrious"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Marie had a bright mind and a __personality?",
+    answer: "lighthearted",
+    options: [
+      "strong",
+      "humorous",
+      "lighthearted",
+      "strange"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Marie ___ by leaving Poland and traveling to France to enter the Sorbonne.",
+    answer: "challenged authority",
+    options: [
+      "behaved",
+      "challenged authority",
+      "was distressed",
+      "showed intelligence"
+    ]
+  },
+    {
+    numb: 4,
+    question: " _____she remembered their joy together.",
+    answer: "Dejectedly",
+    options: [
+      "Worried",
+      "Tearfully",
+      "Happily",
+      "Dejectedly"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Her ____ began to fade when she returned to the Sorbonne to succeed her husband.",
+    answer: "wretchedness",
+    options: [
+      "disappointment",
+      "ambition",
+      "wretchedness",
+      "anger"
+    ]
+  },
+ {
+      numb: 6,
+  question: "When she learned that she could not attend the university in Warsaw, she felt___.",
+  answer: "annoyed",
+  options: [
+   "worried",
+  "depressed",
+  "annoyed",
+  "none of the above"
+ ]
+  },
+];
