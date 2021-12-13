0509918f91acdfd42eaf952617a91f2200f5bde0
SANDEEP K <87005602+sandeepkris2001@users.noreply.github.com>
2021-12-13 17:18:26 +0530

    Update App.js

--- App.js
+++ App.js
@@ -6,9 +6,7 @@ const quiz_box = document.querySelector(".quiz_box");
 const result_box = document.querySelector(".result_box");
 const option_list = document.querySelector(".option_list");
 const viewScore = document.querySelector(".score");
-const time_line = document.querySelector("header .time_line");
-const timeText = document.querySelector(".timer .time_left_txt");
-const timeCount = document.querySelector(".timer .timer_sec");
+const prev_btn = document.querySelector(".prev_btn");
 
 start_btn.onclick = ()=>{
     info_box.classList.add("activeInfo"); 
@@ -27,11 +25,8 @@ continue_btn.onclick = ()=>{
     quiz_box.classList.add("activeQuiz"); 
      showQuestions(0); 
     queCounter(1);
-    startTimer(20); 
-    startTimerLine(0); 
 }
 
-let timeValue =  20;
 let que_count = 0;
 let que_numb = 1;
 let userScore = 0;
@@ -45,19 +40,15 @@ const quit_quiz = result_box.querySelector(".buttons .quit");
 restart_quiz.onclick = ()=>{
     quiz_box.classList.add("activeQuiz"); 
     result_box.classList.remove("activeResult"); 
-    timeValue = 20; 
     que_count = 0;
     que_numb = 1;
     userScore = 0;
-    widthValue = 0;
      showQuestions(que_count); 
     queCounter(que_numb);
     clearInterval(counter); 
     clearInterval(counterLine);
-    startTimer(timeValue); 
-    startTimerLine(widthValue); 
-    timeText.textContent = "Time Left";
     next_btn.classList.remove("show");
+
 }
 
 quit_quiz.onclick = ()=>{
@@ -67,26 +58,44 @@ quit_quiz.onclick = ()=>{
 const next_btn = document.querySelector("footer .next_btn");
 const bottom_ques_counter = document.querySelector("footer .total_que");
 
+
 next_btn.onclick = ()=>{
+    
     if(que_count < questions.length - 1){ 
         que_count++;
         que_numb++; 
          showQuestions(que_count); 
         queCounter(que_numb); 
         clearInterval(counter); 
-        clearInterval(counterLine); 
-        startTimer(timeValue); 
-        startTimerLine(widthValue); 
-        timeText.textContent = "Time Left";
-        if(que_numb==6)
-            next_btn.innerHTML = "Submit";
-    
+       
+        
     }else{
         clearInterval(counter); 
         clearInterval(counterLine);
         showResult(); 
     }
 }
+prev_btn.onclick = ()=>{
+    if(que_count < questions.length){
+        que_count--; 
+        que_numb--; 
+        showQuestions(que_count); 
+        queCounter(que_numb); 
+        clearInterval(counter); 
+        clearInterval(counterLine); 
+    
+        next_btn.classList.remove("show"); 
+        
+    }else{
+        clearInterval(counter); 
+        clearInterval(counterLine); 
+        
+    }
+}
+document.querySelector(".submit_btn").onclick = ()=>{
+    showResult();
+}
+
 function  showQuestions(index){
     const que_text = document.querySelector(".que_text");
 
@@ -148,45 +157,6 @@ function showResult(){
     else{ 
         let scoreTag = '<span>sorry, You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
         scoreText.innerHTML = scoreTag;
-    }
-}
-
-function startTimer(time){
-    counter = setInterval(timer, 1000);
-    function timer(){
-        timeCount.textContent = time; 
-        time--; 
-        if(time < 9){ 
-            let addZero = timeCount.textContent; 
-            timeCount.textContent = "0" + addZero; 
-        }
-        if(time < 0){ 
-            clearInterval(counter);
-            timeText.textContent = "Time Off"; 
-            const allOptions = option_list.children.length; 
-            let correcAns = questions[que_count].answer; 
-            for(i=0; i < allOptions; i++){
-                if(option_list.children[i].textContent == correcAns){
-                    option_list.children[i].setAttribute("class", "option correct"); 
-                    option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag);
-                }
-            }
-            for(i=0; i < allOptions; i++){
-                option_list.children[i].classList.add("disabled");
-            }
-            next_btn.classList.add("show"); 
-        }
-    }
-}
-
-function startTimerLine(time){
-    counterLine = setInterval(timer, 29);
-    function timer(){
-        time += 1; 
-        time_line.style.width = time + "px";
-        if(time > 549){ 
-            clearInterval(counterLine); 
-        }
     }
 }
 
